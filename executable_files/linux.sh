24fc794e4de6ff751b5d7d08639d21ae69b6cf12
ZILOGZ80000 <boarovvoloda@gmail.com>
2025-12-06 16:37:14 +0400

    1.1 тест #5

--- executable_files/linux.sh
+++ executable_files/linux.sh
@@ -3,6 +3,6 @@ sudo apt install -y python3
 sudo apt install -y python3-pip
 git clone https://github.com/ZILOGZ80000/farmer-for-cat-bot
 cd farmer-for-cat-bot
-pip3 install selenium requests Free-Proxy telethon
+pip3 --break-system-packages install selenium requests Free-Proxy telethon
 python3 main.py
 # дальше все сделает скрипт 
